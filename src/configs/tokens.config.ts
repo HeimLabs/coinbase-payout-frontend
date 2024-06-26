f4826bf9ee72a8b5410a237d6b92f1bfe1715565
Van Hohenheim <0xhohenheim@gmail.com>
2024-06-26 18:07:05 +0530

    fix: usdc mainnet token address

--- src/configs/tokens.config.ts
+++ src/configs/tokens.config.ts
@@ -20,7 +20,7 @@ export const tokens = {
         {
             name: "USDC",
             symbol: "$USDC",
-            address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+            address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
         }
     ]
 }
